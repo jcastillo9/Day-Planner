b89b2cb0814503752f66cd28ee59fca412b9f6b0
Janette <janette.castillo91@yahoo.com>
2021-10-30 16:28:45 -0500

    made changes to determineTime function

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,16 +30,17 @@ function storedInput() {
 function determineTime() {
     var hour = time.hours();
     $(".time-block").each(function () {
+
         var currentHour = parseInt($(this).attr("id"));
 
-        if (currentHour > hour) {
-            $(this).addClass("future")
+        if (currentHour < hour) {
+            $(this).addClass("past")
         }
         else if (currentHour === hour) {
             $(this).addClass("present");
         }
         else {
-            $(this).addClass("past");
+            $(this).addClass("future");
         }
     })
 }
